f036d9f59dcda4132a7c86e7f06dc5198fe1fdf8
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-02-22 15:53:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+function changeOne(el){
+	document.getElementById('mlb1').src = "images/blank.png"
+}
+
+function changeTwo(el){
+	document.getElementById('mlb2').src = "images/blank.png"
+}
+
 function nextImage(el){
 	if (){
+		
+	} else if (el.id.match()){
+		el.src = "images/orange_slot.PNG";
+		// if the image is a lemon, change it to an orange!
+	} else if (el.src.match("images/orange_slot.PNG")){
+		el.src = "images/cherry_slot.PNG";
+		// if the image is an orange, change it to a cherry!
+	}
 		// if both mlb logos are clicked, change the baseballs to the letter s!
-	document.getElementById('ball1b').src = "images/s.png"
-	document.getElementById('ball2b').src = "images/s.png"
-	document.getElementById('ball3b').src = "images/s.png"
-	document.getElementById('ball4b').src = "images/s.png"
+	document.getElementById('ball1b').src = "images/s/s.png"
+	document.getElementById('ball2b').src = "images/s/s.png"
+	document.getElementById('ball3b').src = "images/s/s.png"
+	document.getElementById('ball4b').src = "images/s/s.png"
 	} 
 	else {
 		// do nothing
