9d9155f24556e6619c12cc194f2f9b8692b64a88
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-02-22 15:43:17 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function nextImage(el){
 	if (){
 		// if both mlb logos are clicked, change the baseballs to the letter s!
-	document.getElementById('ball1b').src = "image/s.png"
-	document.getElementById('ball2b').src = "image/s.png"
-	document.getElementById('ball3b').src = "image/s.png"
-	document.getElementById('ball4b').src = "image/s.png"
+	document.getElementById('ball1b').src = "images/s.png"
+	document.getElementById('ball2b').src = "images/s.png"
+	document.getElementById('ball3b').src = "images/s.png"
+	document.getElementById('ball4b').src = "images/s.png"
 	} 
 	else {
 		// do nothing
