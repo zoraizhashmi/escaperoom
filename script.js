0c2b5820e45fe032318a19af8939bf0ef4b93a77
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-02-27 15:47:00 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,23 +6,54 @@ function changeTwo(el){
 	document.getElementById('mlb2').src = "images/blank.png"
 }
 
-function nextImage(el){
-	if (){
-		
-	} else if (el.id.match()){
-		el.src = "images/orange_slot.PNG";
-		// if the image is a lemon, change it to an orange!
-	} else if (el.src.match("images/orange_slot.PNG")){
-		el.src = "images/cherry_slot.PNG";
-		// if the image is an orange, change it to a cherry!
+function redHintReveal(el){
+	if (el.src.match("images/mlb.png")){
+		document.getElementById('ball1a').src = "images/blank.png"
+	} else if (el.src.match("images/mlb.png")){
+		document.getElementById('mlb2').src = "images/blank.png"
+	} else if (document.getElementById("mlb1").src.match("images/blank.png") && document.getElementById("mlb2").src.match("images/blank.png")){
+		document.getElementById('ball1b').src = "images/s/s.png"
+		document.getElementById('ball2b').src = "images/s/s.png"
+	} 
+}
+	
+function pinstripleHintReveal(el){
+	if (el.src.match("images/mlb.png")){
+		document.getElementById('mlb1').src = "images/blank.png"
+	} else if (el.src.match("images/mlb.png")){
+		document.getElementById('mlb2').src = "images/blank.png"
+	} else if (document.getElementById("mlb1").src.match("images/blank.png") && document.getElementById("mlb2").src.match("images/blank.png")){
+		document.getElementById('ball1b').src = "images/s/s.png"
+		document.getElementById('ball2b').src = "images/s/s.png"
 	}
-		// if both mlb logos are clicked, change the baseballs to the letter s!
-	document.getElementById('ball1b').src = "images/s/s.png"
-	document.getElementById('ball2b').src = "images/s/s.png"
-	document.getElementById('ball3b').src = "images/s/s.png"
-	document.getElementById('ball4b').src = "images/s/s.png"
+}	
+	
+	function digitHintReveal(el){
+	if (el.src.match("images/mlb.png")){
+		document.getElementById('mlb1').src = "images/blank.png"
+	} else if (el.src.match("images/mlb.png")){
+		document.getElementById('mlb2').src = "images/blank.png"
+	} else if (document.getElementById("mlb1").src.match("images/blank.png") && document.getElementById("mlb2").src.match("images/blank.png")){
+		document.getElementById('ball1b').src = "images/s/s.png"
+		document.getElementById('ball2b').src = "images/s/s.png"
+	} 
+}
+	
+	
+
+function sHintReveal(){
+	if (document.getElementById('mlb1').src.match("images/mlb.png")){
+		document.getElementById('mlb1').src = "images/blank.png"
+	} else if (document.getElementById('mlb2').src.match("images/mlb.png")){
+		document.getElementById('mlb2').src = "images/blank.png"
+		document.getElementById('ball1b').src = "images/s/s.png"
+		document.getElementById('ball2b').src = "images/s/s.png"
+		document.getElementById('ball3b').src = "images/s/s.png"
+		document.getElementById('ball4b').src = "images/s/s.png"
 	} 
 	else {
 		// do nothing
 	}
 }
+
+
