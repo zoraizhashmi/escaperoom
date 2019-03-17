dbd7691ef38434d3faad5662962526f02b720c1b
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2019-03-17 16:47:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//This is a basic function I found in the js which was applied to all of the elements, so I made it so that when any jersey got clicked, the sound of a ball hitting a catcher's glove indicating a strike, or (incorrect). 
+//I guess I added this to make it more interactive. 
 function checkPosition(){
 		var audio = new Audio('sounds/glove.mp3');
 	audio.play();	
 }
 
+// This is just a random function I threw in to make the escape room throw a curveball at the user (no pun intended) upon entering the second page. They would be expecting one page but they got two for the MLB jersey room.
 function sRoomStart() {
 	 alert("But wait... there's more!");
 	mlbTwo()	
 }
 
-
+//This function will allow the user move on to the second part upon clicking the degrom jersey-turned key. 
 function moveOnPartTwo(el){
 	if(el.src.match("images/bosskey.gif")){
 		window.open("part2.html", '_self');
@@ -18,6 +21,7 @@ function moveOnPartTwo(el){
 	audio.play();}
 }
 
+//this function makes the key show if the three proper jerseys are selected and translucent. If not the key will remain 
 function partOnePass(){
 		if (document.getElementById('molina').classList.contains("opaque") && document.getElementById('gallo').classList.contains("opaque") && document.getElementById('freeman').classList.contains("opaque")){
 		var audio = new Audio('sounds/winner.wav');
@@ -27,6 +31,7 @@ function partOnePass(){
 		}
 }
 
+//this function allows for the correct jersey in its row (in part 1) to become less opaque, indicating it is correct. It also forces the user to use the hints, for without the hints triggered the jersey will do nothing
 function rCorrect(el){
 		if (document.getElementById('ball1a').src.match("images/red/red.png") && document.getElementById('ball2a').src.match("images/red/red.png")){
 		el.classList.add("opaque");
@@ -38,6 +43,7 @@ function rCorrect(el){
 		partOnePass();
 }
 
+//this function allows for the correct jersey in its row (in part 1) to become less opaque, indicating it is correct. It also forces the user to use the hints, for without the hints triggered the jersey will do nothing
 function pCorrect(el){
 	if (document.getElementById('ball3a').src.match("images/pinstripes/pinstripe.png") && document.getElementById('ball4a').src.match("images/pinstripes/pinstripe.png")){
 	el.classList.add("opaque"); 
@@ -49,6 +55,7 @@ function pCorrect(el){
 	partOnePass();
 }
 
+//this function allows for the correct jersey in its row (in part 1) to become less opaque, indicating it is correct. It also forces the user to use the hints, for without the hints triggered the jersey will do nothing
 function eCorrect(el){
 	if (document.getElementById('ball5a').src.match("images/even/under10.png") && document.getElementById('ball6a').src.match("images/even/under10.png")){
 	el.classList.add("opaque");
@@ -60,6 +67,7 @@ function eCorrect(el){
 	partOnePass();
 }
 
+//this function allows for the room to be complete once the correct jersey is selected. It also forces the user to use the hints, for without the hints triggered the jersey will do nothing
 function sWinner(el){
 	if (document.getElementById('mlb1').src.match("images/blank.png") && document.getElementById('mlb2').src.match("images/blank.png")){
 	window.open("congrats.html", '_self');
@@ -75,6 +83,7 @@ function sWinner(el){
 	}
 }
 
+//this function allows for the logos in MLB part 1 Row 1 to turn into red blocks, revealing the hint (I also resize the images to make them look nicer) This happens when the MLB logos in the top are both clicked
 function redHintReveal(el){
 		el.src = "images/blank.png";
 	var audio = new Audio('sounds/whoosh.mp3');
@@ -92,7 +101,8 @@ function redHintReveal(el){
 	else {
 	}
 }
-	
+
+//this function allows for the logos in MLB part 1 Row 2 to turn into Pinstripes, revealing the hint (I also resize the images to make them look nicer) This happens when the MLB logos in the middle are both clicked
 function pinstripleHintReveal(el){
 		el.src = "images/blank.png";
 	var audio = new Audio('sounds/whoosh.mp3');
@@ -110,7 +120,8 @@ function pinstripleHintReveal(el){
 	else {
 	}
 }	
-	
+
+//this function allows for the logos in MLB part 1 Row 3 to turn into <10s, revealing the hint (I also resize the images to make them look nicer) This happens when the MLB logos in the last row are both clicked
 	function digitHintReveal(el){
 		el.src = "images/blank.png";
 	var audio = new Audio('sounds/whoosh.mp3');
@@ -130,7 +141,7 @@ function pinstripleHintReveal(el){
 }
 	
 	
-
+//this function allows for the baseballs in MLB part 2 to turn into S's, revealing the hint. This happens when the MLB logos in the middle are both clicked
 function sHintReveal(el){
 	el.src = "images/blank.png";
 	var audio = new Audio('sounds/whoosh.mp3');
@@ -149,38 +160,36 @@ function sHintReveal(el){
 }
 
 
-
+//this is my timer function for my java
 var timeleft = 45;
 var downloadTimer = setInterval(function(){
   document.getElementById("countdown").innerHTML = timeleft;
   timeleft -= 1;
   if(timeleft <= 0){
     clearInterval(downloadTimer);
-	window.open("index.html", '_self');
+	window.open("part1.html", '_self');
 	}
 }, 1000);
 
 	
-/*function startTimer(duration, display) {
+/*
+//This is an unused timer function
+function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
-
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
         display.textContent = minutes + ":" + seconds;
-
         if (--timer < 0) {
            window.history.back();
         }
     }, 1000);
 }
-
 window.onload = function () {
     var oneMinute = 60 * 1,
         display = document.querySelector('#time');
     startTimer(oneMinute, display);
 };
-*/
+*/ 
